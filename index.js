c73582e7cbaeb6040c9e4fb46ced52c2d4847024
mujhepadhnahai <mujhepadhnahe@gmail.com>
2024-12-13 23:16:40 +0530

    frontend integrated

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// Middleware to serve static files
+app.use(express.static('public'));
+
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
 
@@ -13,13 +16,14 @@ const blogs = [
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/blog', (req, res) => {
     res.render('blog', { blogs });
 });
 
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
